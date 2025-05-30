58d91478332ee4be87408eef2213c0200e0bff66
arlansicat7878 <bakazennto>
2025-05-30 09:04:01 +0800

    index

--- app/Http/Controllers/employeecontroller.php
+++ app/Http/Controllers/employeecontroller.php
@@ -11,9 +11,26 @@ class employeecontroller extends Controller
     public function index()
     {   
         $employees = employee::get();
-
+        return view ('employee.index',compact('employees'));
+    }
+    
+    
+    public function create()
+    {   
         return view ('employee.index');
     }
+    
+    public function store(Request $request){
+        $request->validate([
+        'fname' => 'required|max:255|string',
+        'lname' => 'required|max:255|string',
+        'midname' => 'required|max:255|string',
+        'age' => 'required|integer',
+        'address' => 'required|max:255|string',
+        'zip' => 'required|integer',
+        ]);
+
+
 
     // employee::create($request->all());
     // return view ('employee.create');
@@ -23,6 +40,6 @@ class employeecontroller extends Controller
         
             // employee::findOrFail($id)->update($request->all());
             // return redirect ()->back()->with('status','Employee Updated Successfully!');
-            // }
+    }
 
 }
